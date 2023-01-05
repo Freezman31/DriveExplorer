3db8beeff42b9200f5fdedcc466078523389be51
Freezman <60815333+Freezman31@users.noreply.github.com>
2023-01-05 19:32:34 +0100

    Added basic file size system

--- lib/api/drive.dart
+++ lib/api/drive.dart
@@ -5,20 +5,34 @@ import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+const _folderType = "application/vnd.google-apps.folder";
+
 class GoogleDriveApiManager {
   final storage = const FlutterSecureStorage();
   late DriveApi api;
+  late int storageUsed, storageTotal;
 
   Future login() async {
     final credentials = await getCredentials();
     if (credentials == null) {
-      _loginWithBrowser();
+      await _loginWithBrowser();
     } else {
-      final client = Client();
-      authenticatedClient(client, credentials);
+      final client = authenticatedClient(Client(), credentials);
       api = DriveApi(client);
-      client.close();
     }
+
+    final about = await api.about.get($fields: '*');
+    storageUsed = int.parse(about.storageQuota!.usage!);
+    storageTotal = int.parse(about.storageQuota!.limit!);
+    print("Storage used: $storageUsed");
+    print("Storage total: $storageTotal");
+    print("Storage left: ${storageTotal - storageUsed}");
+
+    print("Getting subfolders size");
+    final subfolders = await getSubfoldersSize("root");
+    subfolders.forEach((key, value) {
+      print("$key: $value");
+    });
   }
 
   Future saveCredentials(AccessToken token, String refreshToken) async {
@@ -26,6 +40,13 @@ class GoogleDriveApiManager {
     await storage.write(key: 'data', value: token.data);
     await storage.write(key: 'expiry', value: token.expiry.toString());
     await storage.write(key: 'refreshToken', value: refreshToken);
+  }
+
+  Future deleteCredentials() async {
+    await storage.delete(key: 'type');
+    await storage.delete(key: 'data');
+    await storage.delete(key: 'expiry');
+    await storage.delete(key: 'refreshToken');
   }
 
   Future<AccessCredentials?> getCredentials() async {
@@ -49,7 +70,7 @@ class GoogleDriveApiManager {
     );
   }
 
-  void _lauchAuthInBrowser(Uri url) async {
+  void _launchAuthInBrowser(Uri url) async {
     await canLaunchUrl(url)
         ? await launchUrl(url)
         : throw 'Could not lauch $url';
@@ -62,14 +83,32 @@ class GoogleDriveApiManager {
       env.get('GOOGLE_API_ID'),
       env.get('GOOGLE_API_SECRET'),
     );
-    final client = Client();
 
-    obtainAccessCredentialsViaUserConsent(id, [DriveApi.driveScope], client,
-            (url) => _lauchAuthInBrowser(Uri.parse(url)))
-        .then((AccessCredentials credentials) {
-      api = DriveApi(client);
-      client.close();
-      saveCredentials(credentials.accessToken, credentials.refreshToken!);
+    var authClient =
+        await clientViaUserConsent(id, [DriveApi.driveScope], (url) {
+      //Open Url in Browser
+      _launchAuthInBrowser(Uri.parse(url));
     });
+    await saveCredentials(authClient.credentials.accessToken,
+        authClient.credentials.refreshToken!);
+    api = DriveApi(authClient);
+  }
+
+  Future<Map<String, int>> getSubfoldersSize(String folderId) async {
+    final ret = <String, int>{};
+    final files = await api.files.list(
+      q: "'$folderId' in parents",
+      spaces: 'drive',
+      $fields: '*',
+    );
+    int size = 0;
+    for (final file in files.files!) {
+      if (file.mimeType == _folderType) {
+        ret.addAll(await getSubfoldersSize(file.id!));
+        continue;
+      }
+      ret.addAll({file.name!: int.parse(file.size!)});
+    }
+    return ret;
   }
 }
